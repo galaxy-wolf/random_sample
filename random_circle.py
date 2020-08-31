d25e2cc64fdba6a3b70cc3993af659cbd94d0fac
赵琦 <zhaoqi01@corp.netease.com>
2020-08-31 14:23:30 +0800

    add random

--- random_circle.py
+++ random_circle.py
@@ -1,9 +1,27 @@
 import matplotlib.pyplot as plt
+import random
 fig, ax = plt.subplots()
+
+def random_sample_circle_reject():
+    return (0, 0)
+
+def random_sample_circle_inverse_cdf():
+    return (0, 0)
+
+def random_sample_square():
+    return [random.random()*2.0-1, random.random()*2.0-1]
+
 
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 a_circle = plt.Circle((0, 0), 1.0)
 ax.add_artist(a_circle)
 ax.set_aspect('equal', adjustable='datalim')
+
+NUM = 10
+plist = []
+for i in xrange(0, NUM):
+    plist.append(random_sample_square())
+plt.plot(*zip(*plist), color='r')
+
 plt.show()
